8a80291b0e5c5fa5b9c67b8a028ce8aa825c386b
imallem <iklef.mallem@gmail.com>
2020-05-11 13:55:18 +0200

    database initialization.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,10 @@
+insert into category (description) values ('American');
+insert into category (description) values ('Italian');
+insert into category (description) values ('Mexican');
+insert into category (description) values ('Fast Food');
+insert into unit_of_mesure (description) values ('Teaspoon');
+insert into unit_of_mesure (description) values ('Tablespoon');
+insert into unit_of_mesure (description) values ('cup');
+insert into unit_of_mesure (description) values ('Pinch');
+insert into unit_of_mesure (description) values ('Ounce');
+
